d21b5aa099dd7e7557b38bc3d4bc26c5b41c5ad7
Ryan O'Boyle <wronghanded@protonmail.com>
2020-05-15 04:12:56 -0700

    added delete functionality to admin

---
+++ .history/models/product_20200515035917.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    // @ts-ignore
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+// @ts-ignore
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            // @ts-ignore
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
+
+module.exports = class Product {
+    // @ts-ignore
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+        this.id = Math.random().toString();
+        // @ts-ignore
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
+    }
+
+    // @ts-ignore
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+    // @ts-ignore
+    static findById(id, cb) {
+        // @ts-ignore
+        getProductsFromFile(prodId, product => {
+            // @ts-ignore
+            const product = products.find(p => p.id === id);
+            cb(product);
+        }):
+    }
+
+};
