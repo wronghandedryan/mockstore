9ab60c3e504cd31d67f2f14680a5907654b720bc
Ryan O'Boyle <ryanmacpro@Ryans-MBP.lan>
2020-05-15 01:34:35 -0700

    first real push

---
+++ models/product.js
@@ -0,0 +1,17 @@
+const products = [];
+
+module.exports = class Products {
+        constructor(title) {
+            this.title = t {
+
+            }
+
+            save() {
+                products.pus(this);
+            }
+
+            static.fetchAll() {
+                return.this.proctucts;
+            }
+            // @ts-ignore
+        }
