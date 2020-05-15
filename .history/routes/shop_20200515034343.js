e257338a3eee94a4df339f674f659e74ac30a12f
Ryan O'Boyle <wronghanded@protonmail.com>
2020-05-15 03:50:22 -0700

    adding dynamic routes, extracting dynamic params

--- .history/routes/shop_20200515034343.js
+++ .history/routes/shop_20200515034343.js
@@ -12,6 +12,8 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
+router.get('./products/:productId')
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
--- models/product.js
+++ models/product.js
@@ -1,17 +1,41 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class Products {
-        constructor(title) {
-            this.title = t {
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-            }
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
-            save() {
-                products.pus(this);
-            }
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-            static.fetchAll() {
-                return.this.proctucts;
-            }
-            // @ts-ignore
-        }
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
